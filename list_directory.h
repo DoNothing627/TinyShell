0ec92cfb583cba9457bc408710185a43d12e6336
DoNothing627 <andxh58chn@gmail.com>
2021-11-23 02:18:00 +0700

    first commit

---
+++ list_directory.h
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNING
+#include <dirent.h>
+#include <windows.h>
+#include <stdio.h>
+
+//== ok done ==
+void printListDir(char* directoryName) {
+	DIR* presentDir;
+	struct dirent* directory;
+
+	presentDir = opendir(directoryName);
+	if (presentDir != NULL)
+	{
+		while ((directory = readdir(presentDir)) != NULL) {
+			printf("%s \n", directory->d_name);
+		}
+		closedir(presentDir);
+	}
+	else
+	{
+		printf(" \nError!!! Can't open this directory.");
+	}
+	printf("\n");
+}
